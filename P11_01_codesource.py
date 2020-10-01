7b3a2c3d97d1416e36de8b2f9c5162e2e4aeb7b9
Wanbli83470 <estival.t@hotmail.com>
2020-10-01 21:54:41 +0200

    Fonctionnalité de mise à jour : OK

--- P11_01_codesource.py
+++ P11_01_codesource.py
@@ -317,58 +317,58 @@ class CleaningDB():
             connection.commit()
         return choice_id
 
-# def update():
-#     """Product update function"""
-#     with connection.cursor() as cursor:
-#         sql = "SELECT PRODUITS.ID FROM PRODUITS INNER JOIN SUBSTITUTS ON PRODUITS.ID = SUBSTITUTS.PRODUIT_ID"
-#         cursor.execute(sql, ())
-#         list_id_products = cursor.fetchall()
-#         list_id_products = sql_to_list(sql_=list_id_products)
+def update():
+    """Product update function"""
+    with connection.cursor() as cursor:
+        sql = "SELECT PRODUITS.ID FROM PRODUITS INNER JOIN SUBSTITUTS ON PRODUITS.ID = SUBSTITUTS.PRODUIT_ID"
+        cursor.execute(sql, ())
+        list_id_products = cursor.fetchall()
+        list_id_products = sql_to_list(sql_=list_id_products)
 
-#         """Récupurer nombre produit"""
-#         sql_count = "SELECT MAX(ID) FROM PRODUITS"
-#         cursor.execute(sql_count, ())
-#         max_id = cursor.fetchone()
-#         max_id = max_id.pop("MAX(ID)")
-#         """Récupérer la date en vue des tests"""
-#         sql_get_date = "SELECT `DATE` FROM `PRODUITS` WHERE ID=%s" % max_id
-#         cursor.execute(sql_get_date, ())
-#         sql_get_date = cursor.fetchone()["DATE"]
-#         sql_get_date = str(sql_get_date)
-#         sql_get_date = sql_get_date[:10]
+        """Récupurer nombre produit"""
+        sql_count = "SELECT MAX(ID) FROM PRODUITS"
+        cursor.execute(sql_count, ())
+        max_id = cursor.fetchone()
+        max_id = max_id.pop("MAX(ID)")
+        """Récupérer la date en vue des tests"""
+        sql_get_date = "SELECT `DATE` FROM `PRODUITS` WHERE ID=%s" % max_id
+        cursor.execute(sql_get_date, ())
+        sql_get_date = cursor.fetchone()["DATE"]
+        sql_get_date = str(sql_get_date)
+        sql_get_date = sql_get_date[:10]
         
-#         """Suppression des produits"""
-#         for i in range(1, max_id+1):
-#             if not i in list_id_products:
-#                 sql = "DELETE FROM PRODUITS WHERE ID=%s" % i
-#                 cursor.execute(sql, ())
-#                 connection.commit()
-#         """Reset de l'auto-incrémentation"""
-#         for r in TABLES:
-#             sql = "ALTER TABLE %s AUTO_INCREMENT=0;" % (r)
-#             cursor.execute(sql, ())
-#             connection.commit()
+        """Suppression des produits"""
+        for i in range(1, max_id+1):
+            if not i in list_id_products:
+                sql = "DELETE FROM PRODUITS WHERE ID=%s" % i
+                cursor.execute(sql, ())
+                connection.commit()
+        """Reset de l'auto-incrémentation"""
+        for r in TABLES:
+            sql = "ALTER TABLE %s AUTO_INCREMENT=0;" % (r)
+            cursor.execute(sql, ())
+            connection.commit()
 
-#         """Récupérer les catégories en anglais"""
-#         print(">>> Récupération de vos catégories de produits")
-#         time.sleep(3)
-#         sql_get_category = "SELECT `NOM` FROM `CATEGORIES`"
-#         cursor.execute(sql_get_category, ())
-#         sql_link_category = cursor.fetchall()
-#         sql_link_category = sql_to_list(sql_=sql_link_category)
-#         temp = []
-#         while len(sql_link_category) > 0:
-#             for l in sql_link_category:
-#                 temp.append(CATEGORIES_TO_ENGLISH[sql_link_category.pop()])
+        """Récupérer les catégories en anglais"""
+        print(">>> Récupération de vos catégories de produits")
+        time.sleep(3)
+        sql_get_category = "SELECT `NOM` FROM `CATEGORIES`"
+        cursor.execute(sql_get_category, ())
+        sql_link_category = cursor.fetchall()
+        sql_link_category = sql_to_list(sql_=sql_link_category)
+        temp = []
+        while len(sql_link_category) > 0:
+            for l in sql_link_category:
+                temp.append(CATEGORIES_TO_ENGLISH[sql_link_category.pop()])
 
-#         sql_link_category = temp
-#         del temp
-#         """Télécharger les nouvelles données"""
-#         print(">>> Mise à jour de vos données")
-#         for i in sql_link_category:
-#             DownloadProduct.get_product(max_pages=1, requete=i)
-#         print(f">>> Base de données actualisée le {sql_get_date}")
-#         return sql_get_date
+        sql_link_category = temp
+        del temp
+        """Télécharger les nouvelles données"""
+        print(">>> Mise à jour de vos données")
+        for i in sql_link_category:
+            DownloadProduct.get_product(max_pages=1, requete=i)
+        print(f">>> Base de données actualisée le {sql_get_date}")
+        return sql_get_date
 
 class MainLoop:
     """Main loop of the program"""
